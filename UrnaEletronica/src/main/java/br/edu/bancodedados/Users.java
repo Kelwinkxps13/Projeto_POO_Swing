664bb00971634a590b34e0131cbca50e81f4a57d
Kelwin Jhackson <141432193+Kelwinkxps13@users.noreply.github.com>
2023-10-19 14:12:24 -0300

    Add files via upload
    adicionada a base do projeto

---
+++ UrnaEletronica/src/main/java/br/edu/bancodedados/Users.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.bancodedados;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Users {
+    ArrayList<String> listaUsuarios = new ArrayList<String>();
+}
