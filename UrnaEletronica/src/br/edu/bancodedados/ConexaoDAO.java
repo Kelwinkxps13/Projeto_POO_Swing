f3067dc1c2a5131604d17d2e2db80e7a2b0a2fb8
Kelwin Jhackson <141432193+Kelwinkxps13@users.noreply.github.com>
2023-10-27 15:28:27 -0300

    adicionadas as caracteristicas dos usuarios

---
+++ UrnaEletronica/src/br/edu/bancodedados/ConexaoDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.bancodedados;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aj_un
+ */
+public class ConexaoDAO {
+    
+}
--- UrnaEletronica/src/br/edu/bancodedados/User.java
+++ UrnaEletronica/src/br/edu/bancodedados/User.java
@@ -10,21 +10,35 @@ package br.edu.bancodedados;
  */
 public class User {
     
-    private String nome;
-    private String idade;
-    private String email;
+    protected String nome;
+    protected String senha;
+    protected String email;
+
+    public String getNome() {
+        return nome;
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void setIdade(String idade) {
-        this.idade = idade;
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
+
     
+
     
 }
