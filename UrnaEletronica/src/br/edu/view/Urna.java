ec935793c23be96720b2110ffa870ded1ccf75dc
Lucas Costa Marques <124885548+LucasCostaMrq@users.noreply.github.com>
2023-11-03 09:20:59 -0300

    Add files via upload

--- UrnaEletronica/src/br/edu/view/Urna.java
+++ UrnaEletronica/src/br/edu/view/Urna.java
@@ -8,12 +8,12 @@ package br.edu.view;
  *
  * @author Alunos
  */
-public class Login extends javax.swing.JFrame {
+public class Urna extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
+     * Creates new form Urna
      */
-    public Login() {
+    public Urna() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Urna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Urna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Urna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Urna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new Urna().setVisible(true);
             }
         });
     }
